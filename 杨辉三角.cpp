a9a5f82ff294d1fc226751adf30178dc78413581
XinFeng <104620410+SiMaXinFeng@users.noreply.github.com>
2023-07-18 15:31:10 +0800

    Update 杨辉三角.cpp

--- 杨辉三角.cpp
+++ 杨辉三角.cpp
@@ -2,11 +2,14 @@
 #include<iomanip>
 #include<stdlib.h>
 using namespace std;
+int x;
 int main()
 {
-	int a[11][11];
+	cout<<"请输入行数：";
+	cin>>x;
+	int a[x+1][x+1];
 		a[1][1]=1;
-	for (int i=2;i<=10;++i)
+	for (int i=2;i<=x;++i)
 	{
 		a[i][1]=1; a[i][i]=1;
 		for (int j=2;j<=i-1;++j)
@@ -14,9 +17,9 @@ int main()
 			a[i][j]=a[i-1][j-1]+a[i-1][j];
 		}
 	}
-	for (int i=1;i<=10;++i)
+	for (int i=1;i<=x;++i)
 	{
-		if (i!=10)
+		if (i!=x)
 		{
 			cout<<setw(30-3*i)<<" ";
 		}
